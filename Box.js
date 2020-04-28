aa9c7fd327760918231214c2c97d72c112b7c681
Sadana31 <63460528+Sadana31@users.noreply.github.com>
2020-04-28 12:15:57 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,22 @@
+class Box{
+    constructor(x,y){
+        var options = {
+            'friction':2.0,
+            'density':1.0
+        }
+
+        this.body = Bodies.rectangle(x,y,20,30,options);
+        World.add(world, this.body);
+    }
+
+    display(){
+        var pos =this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0, 0, 30,40);
+        pop();
+    }
+}
